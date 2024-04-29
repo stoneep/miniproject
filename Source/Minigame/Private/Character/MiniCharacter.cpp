9a94b34042aa0235eb15f49f13612f44eae1b4f9
stoneep <blu7archive@gmail.com>
2024-04-30 01:29:25 +0900

    camera

--- Source/Minigame/Private/Character/MiniCharacter.cpp
+++ Source/Minigame/Private/Character/MiniCharacter.cpp
@@ -15,4 +15,25 @@
  	bUseControllerRotationPitch = false;
  	bUseControllerRotationRoll = false;
  	bUseControllerRotationYaw = false;
+
+ 	MovementState = EMovementState::Idle;
  }
+
+void AMiniCharacter::UpdateMovementState()
+{
+ 	if (GetVelocity().SizeSquared() > 0.f)
+ 	{
+ 		MovementState = EMovementState::Moving;
+ 	}
+    else
+    {
+	    MovementState = EMovementState::Idle;
+    }
+ 	
+}
+
+// void AMiniCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+// {
+// 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+//  	
+// }
--- Source/Minigame/Public/Character/MiniCharacter.h
+++ Source/Minigame/Public/Character/MiniCharacter.h
@@ -6,6 +6,14 @@
 #include "Character/MiniCharacterBase.h"
 #include "MiniCharacter.generated.h"
 
+UENUM(BlueprintType)
+enum class EMovementState : uint8
+{
+	Idle,
+	Moving
+};
+
+
 /**
  * 
  */
@@ -16,4 +24,14 @@ class MINIGAME_API AMiniCharacter : public AMiniCharacterBase
 
 public:
 	AMiniCharacter();
+
+	UPROPERTY(BlueprintReadOnly)
+	EMovementState MovementState;
+
+	UFUNCTION(BlueprintCallable)
+	void UpdateMovementState();
+
+// protected:
+// 		virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+// 	
 };
