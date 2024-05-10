c8c315999013640ab64242a1ddea4afd5919f2fd
stoneep <blu7archive@gmail.com>
2024-05-10 19:32:51 +0900

    DefaultAttributes

---
+++ Source/Minigame/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright NexonGames
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
